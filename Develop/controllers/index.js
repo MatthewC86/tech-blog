35b85173eb5f39a5493138cdbefbb4df8218b462
Matthew C <cnymhw86@gmail.com>
2023-02-06 14:12:28 -0500

    added server js

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -14,6 +14,8 @@ router.get('/', async (req, res) => {
     try {
       const blogData = await Blog.findAll();
       const blogs = blogData.map(blog => blog.get({plain: true}));
+
+      res,render('homepage', { blogs });
       
     } catch (err) {
       res.status(404).json(err);      
