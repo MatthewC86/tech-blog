ce6beed8940ef976c5e1cf6efcc8470d9b646383
Matthew C <cnymhw86@gmail.com>
2023-02-06 13:28:44 -0500

    added handlebars and seeds

---
+++ Develop/controllers/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const { Blog } = require('../models');
+// const homeRoutes = require('./home-routes.js');
+// const dashboardRoutes = require('./dashboard-routes.js');
+
+
+router.use('/api', apiRoutes);
+// router.use('/dashboard', dashboardRoutes);
+// router.use('/', homeRoutes);
+
+
+router.get('/', async (req, res) => {
+    try {
+      const blogData = await Blog.findAll();
+      const blogs = blogData.map(blog => blog.get({plain: true}));
+      
+    } catch (err) {
+      res.status(404).json(err);      
+    }
+});
+
+
+module.exports = router;
