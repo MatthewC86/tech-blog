905db7021399b8cf09ab27be71ee1dc9ec1af82f
Matthew C <cnymhw86@gmail.com>
2023-02-07 10:46:04 -0500

    updated controllers

--- Develop/controllers/api/post-routes.js
+++ Develop/controllers/api/post-routes.js
@@ -2,6 +2,15 @@ const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+router.get("/", async (req, res) => {
+    try {
+      const postData = await Post.findAll({});
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 router.post("/", withAuth, async (req, res) => {
   try {
     const postData = await Post.create({
